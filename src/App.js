4dc8cc7ca958e5084a0f5b2cecf931063291cadb
john joseph florina <devjosephflorina@gmail.com>
2020-10-26 11:23:11 +0800

    using arrow function to auto bind handlechange

--- src/App.js
+++ src/App.js
@@ -11,16 +11,15 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
-  }
+  };
   render() {
     const { monsters, searchField } = this.state;
     // shortcut way to use commented code
