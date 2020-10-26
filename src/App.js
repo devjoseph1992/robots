31895e83108d9465622671a592020c7f2acd9136
john joseph florina <devjosephflorina@gmail.com>
2020-10-26 09:29:36 +0800

    card,card-list,css,robo pic api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 // import logo from "./logo.svg";
 import "./App.css";
 
@@ -17,9 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
